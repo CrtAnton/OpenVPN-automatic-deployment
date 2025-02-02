6ddac549b925746be0fbca07eddee6742c6f3e3d
CrtAnton <ca.anton22@gmail.com>
2025-02-02 12:53:28 +0000

    Few changes, most work on master script, not included yet

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,13 +2,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
+data "aws_ami" "ubuntu_latest" {
+  most_recent      = true
+  owners           = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
   filter {
--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "instance_sg" {
 
 
 resource "aws_instance" "openvpn-server" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = data.aws_ami.ubuntu_latest.id
   instance_type          = "t2.micro"  
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
   source_dest_check      = false
